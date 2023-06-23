bd185515e7fd35d5e6b47df1b275a552adcc0a26
tomisalomaa <salomaa.tomi@gmail.com>
2023-06-23 23:56:38 +0300

    merged feature/filter-products-with-search-string

--- Homework.Web/wwwroot/js/site.js
+++ Homework.Web/wwwroot/js/site.js
@@ -1,4 +1,19 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿// Search box Product filtering
+$(document).ready(function () {
+    // activate filter and read input value when key up
+    $("#product-search-box").keyup(function () {
+        var searchString = $("#product-search-box").val().toLowerCase();
+        // go through each product title in card to compare whether it starts with input value and
+        // hide non-matching cards
+        $(".card-text-title").each(function () {
+            var productTitle = $(this).text().trim().toLowerCase();
+            if (!productTitle.startsWith(searchString)) {
+                console.log("Search string: " + searchString + " product: " + productTitle);
+                $(this).closest(".product-card").hide();
+            }
+            else {
+                $(this).closest(".product-card").show();
+            }
+        });
+    });
+});
